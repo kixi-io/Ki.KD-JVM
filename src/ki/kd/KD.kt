bd7dfb6c8d77d45827e6cdd2d6b6932a787138af
Daniel Leuck <dan@ikayzo.com>
2020-07-11 19:36:25 +0900

    Cleaned up warnings, moved formatting base types to Ki.Core

--- src/ki/kd/KD.kt
+++ src/ki/kd/KD.kt
@@ -4,56 +4,11 @@ import java.io.*
 import java.net.URL
 import java.time.format.DateTimeFormatter
 
-
 /**
  * Utility class for KD
  */
 class KD {
     companion object {
-
-        /**
-         *
-         * The SDL standard time format HH:mm:ss.SSS-z
-         *
-         *
-         * Note: SDL uses a 24 hour clock (0-23)
-         *
-         * Note: This is not the same as a duration. This format is used
-         * for the time component of a date_time instance
-         */
-        const val TIME_FORMAT = "HH:mm:ss.SSS-z"
-
-        /**
-         *
-         * The SDL standard date format: yyyy/MM/dd
-         *
-         *
-         * SDL uses the Gregorian calendar.
-         */
-        const val DATE_FORMAT = "yyyy/MM/dd"
-
-        // TODO: Separate patterns for parsing and formatting (formatting should use padding as appropriate)
-        val LOCAL_DATE_FORMATTER = DateTimeFormatter.ofPattern("y/M/d")
-        val LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("y/M/d@H:m:s.nnnnnnnnn")
-        val ZONED_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("y/M/d@H:m:s.nnnnnnnnn/VV")
-
-        /**
-         * The SDL standard DATE_TIME format yyyy/MM/dd-HH:mm:ss.SSS-z
-         *
-         *
-         * Note: SDL uses a 24 hour clock (0-23) and the Gregorian calendar
-         */
-        const val DATE_TIME_FORMAT = DATE_FORMAT + "-" +
-                TIME_FORMAT
-
-        // TODO support all KD types
-        fun format(obj: Any?): String {
-            return when (obj) {
-                null -> "nil"
-                is String -> "\"$obj\""
-                else -> obj.toString()
-            }
-        }
 
         /*
         fun value(text:String) : Any? {
