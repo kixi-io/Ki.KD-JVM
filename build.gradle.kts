fbced64d3db30623f69f731918c581126b614f52
dleuck <dan@ikayzo.com>
2025-12-31 17:36:12 +0900

    KD now has code color support in IntelliJ IDEA. Bug fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "io.kixi"
-version = "2.1.1"
+version = "2.1.2"
 
 repositories {
     mavenCentral()
@@ -15,7 +15,7 @@ kotlin {
 
 dependencies {
     // Ki.Core-JVM via composite build
-    implementation("io.kixi:Ki.Core-JVM:2.1.1")
+    implementation("io.kixi:Ki.Core-JVM:2.1.2")
 
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
